d57078adff81eaf5247137aa09eddf73d7135e89
Elvire Bouvier <e.bouvier@ipnl.in2p3.fr>
2015-08-26 09:30:07 +0200

    only one PseudoTop plugin

--- src/CMS_LesHouches2015.cc
+++ src/CMS_LesHouches2015.cc
@@ -8,7 +8,7 @@
 #include "Rivet/Projections/MergedFinalState.hh"
 #include "Rivet/Tools/ParticleIdUtils.hh"
 
-#include "TopMonteCarlo/RivetTop/interface/PseudoTopLesHouches.hh"
+#include "TopMonteCarlo/RivetTop/interface/PseudoTop.hh"
 
 namespace Rivet {
 
@@ -18,7 +18,7 @@ public:
   }
 
   void init() {
-    addProjection(PseudoTopLesHouches(), "ttbar");
+    addProjection(PseudoTop(), "ttbar");
 
     // Booking of histograms
     _h_njets  = bookHisto1D("jet_mult", 11, -0.5, 10.5);
@@ -73,8 +73,8 @@ public:
     const double weight = event.weight();
 
     // Get the parton level ttbar candidate
-    const PseudoTopLesHouches& ttbar = applyProjection<PseudoTopLesHouches>(event, "ttbar");
-    if (ttbar.mode() != PseudoTopLesHouches::CH_FULLLEPTON) {
+    const PseudoTop& ttbar = applyProjection<PseudoTop>(event, "ttbar");
+    if (ttbar.mode() != PseudoTop::CH_FULLLEPTON) {
       MSG_DEBUG("Event fail channel topology cuts");
       vetoEvent;
     }

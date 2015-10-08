99c2d8468a31ae30f886d0d69f96359a9fa619e1
Junghwan John Goh <jhgoh@cern.ch>
2015-10-08 18:29:33 +0200

    use .yoda standard function

--- test/yodaNormalize.py
+++ test/yodaNormalize.py
@@ -12,13 +12,7 @@ src = yoda.readYODA(sys.argv[1])
 
 out = {}
 for k in src.viewkeys():
-    h = src[k]
-    out[k] = yoda.Scatter2D(h.path, h.title)
-    area = h.integral(True)
-    for b in h.bins:
-        x = (b.xEdges[0]+b.xEdges[1])/2
-        xErr = (b.xEdges[1]-b.xEdges[0])/2
-        y, yErr = b.height, b.heightErr
-        out[k].addPoint(x, y/area, xErr, yErr/area)
+    out[k] = src[k]
+    out[k].normalize()
 
 yoda.writeYODA(out, sys.argv[2])

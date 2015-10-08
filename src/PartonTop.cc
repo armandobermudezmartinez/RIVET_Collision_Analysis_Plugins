9661a47336c91a1fa0de209beebb85b00173a98a
Junghwan John Goh <jhgoh@cern.ch>
2015-10-08 17:28:29 +0200

    skip QCD cluster & strings for speedup, choose highest pt lepton among W daughters (no change expected)

--- src/PartonTop.cc
+++ src/PartonTop.cc
@@ -20,6 +20,7 @@ void PartonTop::project(const Event& e) {
     const int absId = abs(pdgId);
     if ( PID::isHadron(pdgId) ) continue; // skip hadrons
     if ( pdgId == 22 ) continue; // skip photons
+    if ( pdgId == 91 or pdgId == 92 ) continue; // Skip cluster, strings
 
     if ( isZero(p->momentum().perp()) || p->momentum().perp() < ptmin ) continue;
     if ( !inRange(p->momentum().eta(), etamin, etamax) ) continue;
@@ -46,7 +47,6 @@ void PartonTop::project(const Event& e) {
 
     // Build Rivet::Particle
     Particle rp(*p);
-
     // Skip particles from hadronization (and keep tau decay)
     if ( rp.fromDecay() and !rp.hasAncestor(15) and !rp.hasAncestor(-15) ) continue;
 
@@ -69,12 +69,15 @@ void PartonTop::project(const Event& e) {
   }
   if ( isTau1 ) _mode1 = static_cast<DecayMode>(_mode1+3);
   if ( isTau2 ) _mode2 = static_cast<DecayMode>(_mode2+3);
+
 }
 
 Particle PartonTop::findLepton(const ParticleVector& v) const {
+  Particle pp = Particle();
   foreach (const Particle& p, v) {
     const int aid = std::abs(p.pdgId());
-    if ( aid == 11 or aid == 13 ) return p;
+    if ( aid != 11 or aid != 13 ) continue;
+    if ( p.pT() > pp.pT() ) pp = p;
   }
-  return Particle();
+  return pp;
 }

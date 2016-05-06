0b8723aa739aba26d2fc6197a474fdf9f82eba64
dittmer <sjd237@cornell.edu>
2016-05-06 17:04:20 -0500

    PseudoBoostedTop class added to CMS_2015_I1388555 plugin

--- src/CMS_2015_I1388555.cc
+++ src/CMS_2015_I1388555.cc
@@ -1,16 +1,106 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
-/// @todo Include more projections as required, e.g. ChargedFinalState, FastJets, ZFinder...
 #include "Rivet/Math/LorentzTrans.hh"
 #include "Rivet/Tools/ParticleIdUtils.hh"
 
-#include "TopMonteCarlo/RivetTop/interface/PseudoBoostedTop.hh"
+#include "Rivet/Particle.hh"
+#include "Rivet/Particle.fhh"
+#include "Rivet/Event.hh"
+#include "Rivet/Projections/FastJets.hh"
+
+#include <vector>
+#include <string>
+
+
+//#include "TopMonteCarlo/RivetTop/interface/PseudoBoostedTop.hh"
 
 
 namespace Rivet {
 
+  namespace {
 
+    class PseudoBoostedTop : public FinalState {
+    public:
+
+      // Default constructor
+      PseudoBoostedTop(double lepMinPt = 45., double lepMaxEta = 2.1, double jetMinPt = 30., double jetMaxEta = 2.4, double bjetR = 0.5, double tjetR = 0.8)
+        : FinalState(-MAXDOUBLE, MAXDOUBLE, 0*GeV),
+	  _lepMinPt(lepMinPt), _lepMaxEta(lepMaxEta), _jetMinPt(jetMinPt), _jetMaxEta(jetMaxEta), _bjetR(bjetR), _tjetR(tjetR)
+      {
+	setName("PseudoBoostedTop");
+      }
+      
+      enum DecayMode {CH_HADRON, CH_MUON, CH_ELECTRON, CH_TAU};
+      enum TTbarMode {CH_DILEP, CH_ALLHADRON, CH_SEMILEP_MU, CH_SEMILEP_EL, CH_SEMILEP_TAU};
+      
+      TTbarMode mode() const {
+        if ((_topDecay == CH_MUON && _antitopDecay == CH_HADRON) || (_topDecay == CH_HADRON && _antitopDecay == CH_MUON)) return CH_SEMILEP_MU;
+        else if ((_topDecay == CH_ELECTRON && _antitopDecay == CH_HADRON) || (_topDecay == CH_HADRON && _antitopDecay == CH_ELECTRON)) return CH_SEMILEP_EL;
+        else if ((_topDecay == CH_TAU && _antitopDecay == CH_HADRON) || (_topDecay == CH_HADRON && _antitopDecay == CH_TAU)) return CH_SEMILEP_TAU;
+        else if (_topDecay == CH_HADRON && _antitopDecay == CH_HADRON) return CH_ALLHADRON;
+        else return CH_DILEP;
+      }
+      
+      DecayMode topDecay() const { return _topDecay;}
+      DecayMode antitopDecay() const {return _antitopDecay;}
+      
+      /// Clone on the heap.
+      virtual const Projection* clone() const {
+        return new PseudoBoostedTop(*this);
+      }
+      
+      //@}
+      
+    public:
+      Particle partonTop() const   {
+        if (_antitopDecay == CH_HADRON) return _antitop;
+        else if (_topDecay == CH_HADRON) return _top;
+        else return NULL;
+      }
+      float mtt() const {return _mtt;}
+      Jet particleTop() const      {return _particleTop;}
+      
+      bool isParticleLep() const {return _isParticleLep;}
+      bool isGenBJet() const {return _isGenBJet;}
+      bool isGenTopJet() const {return _isGenTopJet;}
+      bool passParticle() const    {return _passParticle;}
+      
+    protected:
+      // Apply the projection to the event
+      void project(const Event& e) override;
+      bool isFromTop(const GenParticle* p);
+      double getDeltaR(const FourMomentum& p1, const FourMomentum& p2);
+      bool fromHardMuon(const GenParticle* p);
+      
+    private:
+      const double _lepMinPt, _lepMaxEta, _jetMinPt, _jetMaxEta;
+      const double _bjetR, _tjetR;
+      
+      constexpr static double _halfpi = 3.1415 / 2.;
+      
+    private:
+      bool _isValid;
+      
+      DecayMode _topDecay;
+      DecayMode _antitopDecay;
+      
+      Particle _top;
+      Particle _antitop;
+      
+      Jet _particleTop;
+      
+      bool _isParticleLep;
+      bool _isGenBJet;
+      bool _isGenTopJet;
+      bool _passParticle;
+      
+      float _mtt;
+      
+    };
+    
+  }
+  
   class CMS_2015_I1388555 : public Analysis {
     public:
 
@@ -18,7 +108,6 @@ namespace Rivet {
       CMS_2015_I1388555()
         : Analysis("CMS_2015_I1388555")
       {    }
-
 
       /// @name Analysis methods
       //@{
@@ -337,9 +426,191 @@ namespace Rivet {
   };
 
 
-
   // The hook for the plugin system
   DECLARE_RIVET_PLUGIN(CMS_2015_I1388555);
 
-
+  namespace {
+    struct GreaterByPt
+    {
+      bool operator()(const Particle& a, const Particle& b) {
+	return a.pt() > b.pt();
+      }
+    };
+    
+    struct GreaterByPtJet
+    {
+      bool operator()(const Jet& a, const Jet& b) {
+	return a.momentum().pt() > b.momentum().pt();
+      }
+    };
+    
+    double PseudoBoostedTop::getDeltaR(const FourMomentum& p1, const FourMomentum& p2){
+      double deta = std::fabs(p1.eta() - p2.eta());
+      double dphi = std::fabs(p1.phi() - p2.phi());
+      return std::sqrt(deta * deta + dphi * dphi);
+    }
+    
+    // Determine if lepton comes from hard top decay
+    bool PseudoBoostedTop::isFromTop(const GenParticle* p){
+      bool fromTop = false;
+      const int pdgId = p->pdg_id();
+      GenVertex* prodVtx = p->production_vertex();
+      if (prodVtx != NULL) {
+	foreach (const GenParticle* p1, Rivet::particles(prodVtx, HepMC::parents)) {
+	  const int pdgIdParent = p1->pdg_id();
+	  if (pdgIdParent == 24) { // parent is a W+ --> leptonic top
+	    if (pdgId == -11) _topDecay = CH_ELECTRON;
+	    if (pdgId == -13) _topDecay = CH_MUON;
+	    if (pdgId == -15) _topDecay = CH_TAU;
+	    fromTop = true;
+	  }
+	  else if (pdgIdParent == -24) { // parent is a W- --> leptonic antitop
+	    if (pdgId == 11) _antitopDecay = CH_ELECTRON;
+	    if (pdgId == 13) _antitopDecay = CH_MUON;
+	    if (pdgId == 15) _antitopDecay = CH_TAU;
+	    fromTop = true;
+	  }
+	}
+      }
+      return fromTop;  
+    }
+    
+    bool PseudoBoostedTop::fromHardMuon(const GenParticle* p){
+      GenVertex* prodVtx = p->production_vertex();
+      if (prodVtx == NULL) return false;
+      foreach (const GenParticle* ancestor, Rivet::particles(prodVtx, HepMC::ancestors)) {
+	if (std::abs(ancestor->pdg_id()) == 13 && ancestor->status() == 3) return true;
+      }
+      return false;
+    }
+    
+    void PseudoBoostedTop::project(const Event& e) {
+      // Lepton : genParticle
+      // (b) jets: anti-kt R=0.5 using all final-state particles excluding neutrinos
+      // top jets: CA R=0.8 using all particles excluding neutrinos with 140 < mjet < 250
+      
+      _isValid = false;
+      _theParticles.clear();
+      _topDecay = CH_HADRON;
+      _antitopDecay = CH_HADRON;
+      _isParticleLep = false;
+      _isGenBJet = false;
+      _isGenTopJet = false;
+      _passParticle = false;
+      
+      _mtt = -1.;
+      
+      // Get parton-level top / state
+      Particles pForJet;
+      
+      Particle refLep;
+      
+      std::set<int> pFromMuon;
+      
+      foreach (const GenParticle* p, Rivet::particles(e.genEvent())) {
+	const int status = p->status();
+	const int pdgId = p->pdg_id();
+	
+	Particle rp(*p);
+	
+	// Get top quarks
+	if (pdgId == 6 && (status == 3 || status == 22)){
+	  _top = rp;
+	}
+	if (pdgId == -6 && (status == 3 || status == 22)){
+	  _antitop = rp;
+	}
+	
+	// Get electrons
+	if (std::abs(pdgId) == 11){
+	  if (isFromTop(p)) {
+	    refLep = rp;
+	  }      
+	}
+	
+	// Get muons
+	if (std::abs(pdgId) == 13){
+	  if (isFromTop(p)) {
+	    refLep = rp;
+	  }      
+	}
+	
+	// Get taus
+	if (std::abs(pdgId) == 15){
+	  if (isFromTop(p)) {
+	    refLep = rp;
+	  }      
+	}
+	
+	if (status == 1) {
+	  if (fromHardMuon(p)) {
+	    pFromMuon.insert(p->barcode());
+	  }
+	}
+      }
+      
+      const FourMomentum& ttbarP4 = _top.momentum() + _antitop.momentum();
+      _mtt = ttbarP4.mass();
+      
+      // Now start with particle-level quantities
+      
+      // Get particles for jet clustering
+      foreach (const GenParticle* p, Rivet::particles(e.genEvent())) {
+	const int status = p->status();
+	const int barcode = p->barcode();
+	Particle rp(*p);
+	if (status == 1 && !rp.isNeutrino() && pFromMuon.find(barcode) == pFromMuon.end()) { //consider all final state particles except neutrinos and decay products of hard muon
+	  pForJet.push_back(rp);
+	} 
+      }
+      
+      // In case the clustering alters the particle collection, clone it
+      Particles pForBjet = pForJet;
+      Particles pForTjet = pForJet;
+      
+      // Then do the AK5 clustering
+      FastJets ak5Jet(FastJets::ANTIKT, _bjetR);
+      ak5Jet.calc(pForBjet);
+      
+      // Then do the CA8 clustering
+      FastJets ca8Jet(FastJets::CAM, _tjetR);
+      ca8Jet.calc(pForTjet);
+      
+      if (refLep.momentum().pt() > _lepMinPt && std::fabs(refLep.eta()) < _lepMaxEta){
+	
+	_isParticleLep = true;
+	const FourMomentum& refLepP4 = refLep.momentum();
+	
+	Jets genBjets;
+	Jets genTjets;
+	int nGenBjets = 0;
+	int nGenTjets = 0;
+	
+	foreach (const Jet& jet, ak5Jet.jetsByPt(_jetMinPt)) {
+	  if (std::fabs(jet.eta()) > _jetMaxEta) continue;
+	  if (getDeltaR(jet.momentum(),refLepP4) > _halfpi) continue;
+	  if (getDeltaR(jet.momentum(),refLepP4) < 0.1) continue;
+	  genBjets.push_back(jet);
+	  nGenBjets += 1;
+	}
+	
+	foreach (const Jet& jet, ca8Jet.jetsByPt(_jetMinPt)) {
+	  if (std::fabs(jet.eta()) > _jetMaxEta) continue;
+	  if (getDeltaR(jet.momentum(), refLepP4) < _halfpi) continue;
+	  if (jet.momentum().mass() < 140.) continue;
+	  if (jet.momentum().mass() > 250.) continue;
+	  genTjets.push_back(jet);
+	  nGenTjets += 1;
+	}
+	
+	if (genTjets.size() > 1) std::sort(genTjets.begin(), genTjets.end(), GreaterByPtJet());
+	
+	if (nGenBjets >= 1) _isGenBJet = true;
+	if (nGenBjets >= 1 && nGenTjets >= 1) _isGenTopJet = true;
+	if (_isGenTopJet == true) _particleTop = genTjets[0];
+	if (_particleTop.momentum().pT() > 400.) _passParticle = true;
+      }
+      _isValid = true;
+    } 
+  }
 }

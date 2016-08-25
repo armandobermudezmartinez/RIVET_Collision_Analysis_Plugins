4ac3b0651683c88e22c66e480c673023277e30d8
Markus Seidel <markus.seidel@cern.ch>
2016-08-25 17:43:00 +0200

    Changes by Markus Radziej

--- src/CMS_2016_I1473674.cc
+++ src/CMS_2016_I1473674.cc
@@ -17,141 +17,133 @@ namespace Rivet {
   class CMS_2016_I1473674 : public Analysis {
   public:
 
-    /// Minimal constructor
-    CMS_2016_I1473674() : Analysis("CMS_2016_I1473674")
-    {
+    // Minimal constructor
+    CMS_2016_I1473674() : Analysis("CMS_2016_I1473674") {
     }
 
-
-    /// @name Analysis methods
-    //@{
-
-    /// Set up projections and book histograms
+    // Set up projections and book histograms
     void init() {
       // Complete final state
-      double MAXRAPIDITY = 1e10;
-      FinalState fs(-MAXRAPIDITY, MAXRAPIDITY, 0*GeV);
-      
+      FinalState fs(-MAXDOUBLE, MAXDOUBLE, 0*GeV);
+
       // Projection for taus
       TauFinder taus(TauFinder::ANY);
       addProjection(taus, "Tau");
       IdentifiedFinalState nu_taus(fs);
       nu_taus.acceptIdPair(PID::NU_TAU);
-      addProjection(nu_taus, "nu_tau");
+      addProjection(nu_taus, "NuTau");
 
       // Projection for electrons and muons
       IdentifiedFinalState photons(fs);
       photons.acceptIdPair(PID::PHOTON);
-      
+
       IdentifiedFinalState el_id(fs);
       el_id.acceptIdPair(PID::ELECTRON);
       PromptFinalState electrons(el_id);
       addProjection(electrons, "Electrons");
-      DressedLeptons dressedelectrons(photons, electrons, 0.1, Cuts::open(), true, false);
-      addProjection(dressedelectrons, "DressedElectrons");
-      
+      DressedLeptons dressed_electrons(photons, electrons, 0.1, Cuts::open(), true, false);
+      addProjection(dressed_electrons, "DressedElectrons");
+
       IdentifiedFinalState mu_id(fs);
       mu_id.acceptIdPair(PID::MUON);
       PromptFinalState muons(mu_id);
       addProjection(muons, "Muons");
-      DressedLeptons dressedmuons(photons, muons, 0.1, Cuts::open(), true, false);
-      addProjection(dressedmuons, "DressedMuons");
-      
+      DressedLeptons dressed_muons(photons, muons, 0.1, Cuts::open(), true, false);
+      addProjection(dressed_muons, "DressedMuons");
+
       // Projection for jets
-      VetoedFinalState fsForJets(FinalState(-MAXRAPIDITY, MAXRAPIDITY, 0*GeV));
-      fsForJets.addVetoOnThisFinalState(dressedmuons);
-      addProjection(FastJets(fsForJets, FastJets::ANTIKT, 0.5), "Jets");
-      
+      VetoedFinalState fs_jets(FinalState(-MAXDOUBLE, MAXDOUBLE, 0*GeV));
+      fs_jets.addVetoOnThisFinalState(dressed_muons);
+      addProjection(FastJets(fs_jets, FastJets::ANTIKT, 0.5), "Jets");
+
       // Projections for MET
       addProjection(MissingMomentum(), "MET");
-      
+
       // Weight counter
       _vis_unit_weights = 0.;
 
-      // Booking of histograms      
-      _h_met = bookHisto1D(5, 1, 1);
-      _h_ht  = bookHisto1D(6, 1, 1);
-      _h_st  = bookHisto1D(7, 1, 1);
-      _h_wpt = bookHisto1D(8, 1, 1);
+      // Booking of histograms
+      _hist_met = bookHisto1D(5, 1, 1);
+      _hist_ht  = bookHisto1D(6, 1, 1);
+      _hist_st  = bookHisto1D(7, 1, 1);
+      _hist_wpt = bookHisto1D(8, 1, 1);
     }
 
 
+    // per event analysis
     void analyze(const Event& event) {
       const double weight = event.weight();
-      
-      // select ttbar -> lepton+jets without taus
-      const DressedLeptons& dressedelectrons = applyProjection<DressedLeptons>(event, "DressedElectrons");
-      const DressedLeptons& dressedmuons = applyProjection<DressedLeptons>(event, "DressedMuons");
-      if (dressedelectrons.dressedLeptons().size() + dressedmuons.dressedLeptons().size() != 1) vetoEvent;
-      
+
+      // select ttbar -> lepton+jets
+      const DressedLeptons& dressed_electrons = applyProjection<DressedLeptons>(event, "DressedElectrons");
+      const DressedLeptons& dressed_muons = applyProjection<DressedLeptons>(event, "DressedMuons");
+      if (dressed_electrons.dressedLeptons().size() +
+          dressed_muons.dressedLeptons().size() != 1)
+        vetoEvent;
+
       FourMomentum lepton;
-      if (dressedelectrons.dressedLeptons().size() == 1) lepton = dressedelectrons.dressedLeptons()[0].momentum();
-      else lepton = dressedmuons.dressedLeptons()[0].momentum();
-      
+      if (dressed_electrons.dressedLeptons().size() == 1) {
+        lepton = dressed_electrons.dressedLeptons()[0].momentum();
+      } else {
+        lepton = dressed_muons.dressedLeptons()[0].momentum();
+      }
+
+      // veto if lepton is tau
       const TauFinder& taus = applyProjection<TauFinder>(event, "Tau");
-      const IdentifiedFinalState nu_taus = applyProjection<IdentifiedFinalState>(event, "nu_tau");
-      foreach(const Particle& tau, taus.taus()) {
-        foreach(const Particle& nu, nu_taus.particles()) {
-          if (tau.pid() * nu.pid() < 0) continue;
-          const FourMomentum wCandidate = tau.momentum() + nu.momentum();
-          if (abs(wCandidate.mass() - 80.4) > 5.) vetoEvent;
+      const IdentifiedFinalState nu_taus = applyProjection<IdentifiedFinalState>(event, "NuTau");
+      foreach (const Particle& tau, taus.taus()) {
+        foreach (const Particle& nu, nu_taus.particles()) {
+          if (tau.pid() * nu.pid() < 0)
+            continue;
+
+          const FourMomentum w_candidate = tau.momentum() + nu.momentum();
+          if (abs(w_candidate.mass() - 80.4) > 5.)
+            vetoEvent;
         }
       }
 
       // count weights in visible phase space
-      if (weight != 0.) _vis_unit_weights += weight/std::abs(weight);
-      
+      if (weight != 0.)
+        _vis_unit_weights += weight / std::abs(weight);
+
       // MET
       const MissingMomentum& met = applyProjection<MissingMomentum>(event, "MET");
-      _h_met->fill(met.visibleMomentum().pT()/GeV, weight);
-      
+      _hist_met->fill(met.visibleMomentum().pT()/GeV, weight);
+
       // HT and ST
       const FastJets& jetpro = applyProjection<FastJets>(event, "Jets");
       const Jets jets = jetpro.jetsByPt(20*GeV);
-      
+
       double ht = 0.0;
       foreach (const Jet& j, jets) {
         if (deltaR(j.momentum(), lepton) > 0.3) {
           ht += j.pT();
         }
       }
+
       double st = ht + lepton.pT() + met.visibleMomentum().pT();
-      _h_ht->fill(ht/GeV, weight);
-      _h_st->fill(st/GeV, weight);
-      
+      _hist_ht->fill(ht/GeV, weight);
+      _hist_st->fill(st/GeV, weight);
+
       // WPT
       FourMomentum w = lepton - met.visibleMomentum();
-      _h_wpt->fill(w.pT()/GeV, weight);
+      _hist_wpt->fill(w.pT()/GeV, weight);
     }
 
-
+    // scale by 1 over weight
     void finalize() {
-      const double s = 1./_vis_unit_weights;
-      scale(_h_met, s);
-      scale(_h_ht, s);
-      scale(_h_st, s);
-      scale(_h_wpt, s);
+      const double s = 1. / _vis_unit_weights;
+      scale(_hist_met, s);
+      scale(_hist_ht, s);
+      scale(_hist_st, s);
+      scale(_hist_wpt, s);
     }
-
-    //@}
-
 
   private:
-
-    // @name Histogram data members
-    //@{
-    
     double _vis_unit_weights;
-    
-    Histo1DPtr _h_met, _h_ht, _h_st, _h_wpt;
-
-    //@}
-
+    Histo1DPtr _hist_met, _hist_ht, _hist_st, _hist_wpt;
   };
-
-
 
   // The hook for the plugin system
   DECLARE_RIVET_PLUGIN(CMS_2016_I1473674);
-
 }

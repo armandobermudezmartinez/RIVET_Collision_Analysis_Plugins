518215786eb1ba1a0a36ef45c28dd2c2ddd2c149
Markus Seidel <markus.seidel@cern.ch>
2017-01-12 10:13:02 +0100

    Options for test configs (maxEvents, runOnly, output)

--- test/runRivetAnalyzer_13TeV_cfg.py
+++ test/runRivetAnalyzer_13TeV_cfg.py
@@ -1,4 +1,11 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+options = VarParsing.VarParsing ('standard')
+options.register('runOnly', '', VarParsing.VarParsing.multiplicity.singleton,VarParsing.VarParsing.varType.string, "Run only specified analysis")
+options.setDefault('output', 'test.yoda')
+options.parseArguments()
+print options
 
 process = cms.Process("runRivetAnalysis")
 
@@ -7,7 +14,7 @@ process.load('Configuration.StandardSequences.Services_cff')
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(options.maxEvents))
 
 process.source = cms.Source("EmptySource")
 
@@ -36,14 +43,18 @@ process.generator = cms.EDFilter("Pythia8GeneratorFilter",
 
 process.load("GeneratorInterface.RivetInterface.rivetAnalyzer_cfi")
 
-process.rivetAnalyzer.AnalysisNames = cms.vstring(
-    'CMS_2016_I1434354', # diff xs lepton+jets
-    'MC_TTBAR_HADRON', # MC analysis for lepton+jets
-    'CMS_LesHouches2015', # MC analysis for dilepton
-    'MC_GENERIC', # MC generic analysis
-    'MC_XS', # MC xs analysis
-)
-process.rivetAnalyzer.OutputFile      = "test.yoda"
+if options.runOnly:
+    process.rivetAnalyzer.AnalysisNames = cms.vstring(options.runOnly)
+else:
+    process.rivetAnalyzer.AnalysisNames = cms.vstring(
+        'CMS_2016_I1434354', # diff xs lepton+jets
+        'MC_TTBAR_HADRON', # MC analysis for lepton+jets
+        'CMS_LesHouches2015', # MC analysis for dilepton
+        'MC_GENERIC', # MC generic analysis
+        'MC_XS', # MC xs analysis
+    )
+
+process.rivetAnalyzer.OutputFile      = options.output
 process.rivetAnalyzer.HepMCCollection = cms.InputTag("generator:unsmeared")
 process.rivetAnalyzer.CrossSection    = 831.76 # NNLO (arXiv:1303.6254)
 

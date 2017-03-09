018d1dc629d044992e8c7369d7e314a051cdc90e
xjanssen <xavier.janssen@cern.ch>
2017-03-09 18:34:37 +0100

    Added CMS_2015_I1380605

--- rivetSetup.sh
+++ rivetSetup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 source $CMSSW_BASE/src/GeneratorInterface/RivetInterface/test/rivetSetup.sh
 
-GROUPS=(Top FSQ)
-for GROUP in ${GROUPS[@]}
+GROUPS='Top FSQ'
+#for GROUP in ${GROUPS[@]}
+for GROUP in 'FSQ'
 do
+  echo $GROUP
   export RIVET_REF_PATH=$RIVET_REF_PATH:$CMSSW_BASE/src/Rivet/${GROUP}/data
   export RIVET_INFO_PATH=$RIVET_INFO_PATH:$CMSSW_BASE/src/Rivet/${GROUP}/data
   export RIVET_PLOT_PATH=$RIVET_PLOT_PATH:$CMSSW_BASE/src/Rivet/${GROUP}/data

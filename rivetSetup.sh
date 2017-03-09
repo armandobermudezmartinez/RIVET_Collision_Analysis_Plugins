de50005de1aa2d6f93286f96bfa8b9f544abe1f4
Sebastian <sebastian.baur@kit.edu>
2017-03-09 19:44:59 +0100

    modify rivetSetup.sh to work for me

--- rivetSetup.sh
+++ rivetSetup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 source $CMSSW_BASE/src/GeneratorInterface/RivetInterface/test/rivetSetup.sh
 
-GROUPS=(Top FSQ)
-for GROUP in ${GROUPS[@]}
+for GROUP in Top FSQ
 do
   export RIVET_REF_PATH=$RIVET_REF_PATH:$CMSSW_BASE/src/Rivet/${GROUP}/data
   export RIVET_INFO_PATH=$RIVET_INFO_PATH:$CMSSW_BASE/src/Rivet/${GROUP}/data

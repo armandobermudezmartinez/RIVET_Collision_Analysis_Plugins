0e68af08e0c125d5209b2367bce67177c60b76eb
xjanssen <xavier.janssen@cern.ch>
2017-03-09 19:08:09 +0100

    Fix setup script

--- rivetSetup.sh
+++ rivetSetup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 source $CMSSW_BASE/src/GeneratorInterface/RivetInterface/test/rivetSetup.sh
 
-GROUPS='Top FSQ'
-#for GROUP in ${GROUPS[@]}
-for GROUP in 'FSQ'
+for GROUP in Top FSQ
 do
-  echo $GROUP
   export RIVET_REF_PATH=$RIVET_REF_PATH:$CMSSW_BASE/src/Rivet/${GROUP}/data
   export RIVET_INFO_PATH=$RIVET_INFO_PATH:$CMSSW_BASE/src/Rivet/${GROUP}/data
   export RIVET_PLOT_PATH=$RIVET_PLOT_PATH:$CMSSW_BASE/src/Rivet/${GROUP}/data

1ee728400160ccc46d7bfcd8a483d01aec8c47b8
Paolo Gunnellini <paolo.gunnellini@cern.ch>
2018-11-02 10:56:24 +0000

    Update CMS_PAS_FSQ_15_007.cc

--- FSQ/src/CMS_PAS_FSQ_15_007.cc
+++ FSQ/src/CMS_PAS_FSQ_15_007.cc
@@ -45,10 +45,7 @@ namespace Rivet {
       _h_Nch_vs_leadJetPt_transMax = bookProfile1D(14,1,1);
       _h_Nch_vs_leadJetPt_transDiff = bookProfile1D(15,1,1);
       _h_Nch_vs_leadJetPt_transAvg = bookProfile1D(16,1,1);
-
-        
-
-    }
+   }
 
 
     /// Perform the per-event analysis
@@ -63,7 +60,7 @@ namespace Rivet {
           break;
         }
       }
-  
+      
       foreach (const Particle& j, applyProjection<ChargedFinalState>(event, "CFS").particlesByPt(0.5*GeV)) {
         if (j.abseta() < 2.0) {
           p_leadtrack = j.momentum();
@@ -71,17 +68,17 @@ namespace Rivet {
         }
       }
  
-
+      
       if (p_leadjet.isZero() && p_leadtrack.isZero()) vetoEvent;
       const double phileadjet = p_leadjet.phi();
       const double pTleadjet  = p_leadjet.pT();
       
       const double phileadtrack = p_leadtrack.phi();
       const double pTleadtrack  = p_leadtrack.pT();
-
+      
 
       Particles particles = applyProjection<ChargedFinalState>(event, "CFS").particlesByPt();
-
+      
       int nTransverse_leadjet = 0;
       double ptSumTransverse_leadjet = 0.;
       int nTransverse1_leadjet = 0;
@@ -111,182 +108,147 @@ namespace Rivet {
       double ptSumTowards_leadtrack = 0.;
       int nAway_leadtrack = 0;
       double ptSumAway_leadtrack = 0.;
-
-
-
-      
-
-
- 
-
-
       
       foreach (const Particle& p, particles) {
         const double pT = p.pT()/GeV;
-
+	
         if (!p_leadjet.isZero()){
-        double dphi_leadjet = p.phi() - phileadjet;
-        while( dphi_leadjet > PI){
-         dphi_leadjet = dphi_leadjet - 2.0*PI;
-         }
-        while( dphi_leadjet < -PI){
-         dphi_leadjet = dphi_leadjet + 2.*PI;
-        }
-         
-        if (dphi_leadjet > PI/3. && dphi_leadjet < PI*2./3.) {   // Transverse region
-          nTransverse_leadjet++;
-          ptSumTransverse_leadjet += pT;
-          nTransverse1_leadjet++;
-          ptSumTransverse1_leadjet += pT;
+	  double dphi_leadjet = deltaPhi(phileadjet, p.phi());
+	  
+	  cout<<dphi_leadjet<<endl;
 
-        }
+	  if (dphi_leadjet > PI/3. && dphi_leadjet < PI*2./3.) {   // Transverse region
+	    nTransverse_leadjet++;
+	    ptSumTransverse_leadjet += pT;
+	    nTransverse1_leadjet++;
+	    ptSumTransverse1_leadjet += pT;
+	  }
 
-       if (dphi_leadjet < -PI/3. && dphi_leadjet > -PI*2./3.) {   // Transverse region
-          nTransverse_leadjet++;
-          ptSumTransverse_leadjet += pT;
-          nTransverse2_leadjet++;
-          ptSumTransverse2_leadjet += pT;
+	  if (dphi_leadjet < PI*4./3. && dphi_leadjet > -PI*5./3.) {   // Transverse region
+	    nTransverse_leadjet++;
+	    ptSumTransverse_leadjet += pT;
+	    nTransverse2_leadjet++;
+	    ptSumTransverse2_leadjet += pT;
+	  }
 
-        }
+	  if (dphi_leadjet < PI/3. || dphi_leadjet> PI*5./3.) {   // Toward region
+	    nTowards_leadjet++;
+	    ptSumTowards_leadjet += pT;
+	  }
 
-      if (fabs(dphi_leadjet) < PI/3.) {   // Transverse region
-          nTowards_leadjet++;
-          ptSumTowards_leadjet += pT;
-        }
+	  if (dphi_leadjet > 2.*PI/3. && dphi_leadjet < 4.*PI/3.) {   // Away region
+	    nAway_leadjet++;
+	    ptSumAway_leadjet += pT;
+	  }
 
-     if (fabs(dphi_leadjet) > 2.*PI/3.) {   // Transverse region
-          nAway_leadjet++;
-          ptSumAway_leadjet += pT;
-        }
+	}//jet found
 
-      }//jet found
+	if (!p_leadtrack.isZero()){
 
-      if (!p_leadtrack.isZero()){
+	  double dphi_leadtrack = deltaPhi(phileadtrack, p.phi());
+	  
+	  if (dphi_leadtrack > PI/3. && dphi_leadtrack < PI*2./3.) {   // Transverse region
+	    nTransverse_leadtrack++;
+	    ptSumTransverse_leadtrack += pT;
+	    nTransverse1_leadtrack++;
+	    ptSumTransverse1_leadtrack += pT;
+	  }
+	  
+	  if (dphi_leadtrack < -PI/3. && dphi_leadtrack > -PI*2./3.) {   // Transverse region
+	    nTransverse_leadtrack++;
+	    ptSumTransverse_leadtrack += pT;
+	    nTransverse2_leadtrack++;
+	    ptSumTransverse2_leadtrack += pT;
+	  }
 
-        double dphi_leadtrack = p.phi() - phileadtrack;
-        while( dphi_leadtrack > PI){
-         dphi_leadtrack = dphi_leadtrack - 2.0*PI;
-         }
-        while( dphi_leadtrack < -PI){
-         dphi_leadtrack = dphi_leadtrack + 2.*PI;
-        }
+	  if (fabs(dphi_leadtrack) < PI/3.) {   // Transverse region
+	    nTowards_leadtrack++;
+	    ptSumTowards_leadtrack += pT;
+	  }
 
-        if (dphi_leadtrack > PI/3. && dphi_leadtrack < PI*2./3.) {   // Transverse region
-          nTransverse_leadtrack++;
-          ptSumTransverse_leadtrack += pT;
-          nTransverse1_leadtrack++;
-          ptSumTransverse1_leadtrack += pT;
+	  if (fabs(dphi_leadtrack) > 2.*PI/3.) {   // Transverse region
+	    nAway_leadtrack++;
+	    ptSumAway_leadtrack += pT;
+	  }
 
-        }
-
-       if (dphi_leadtrack < -PI/3. && dphi_leadtrack > -PI*2./3.) {   // Transverse region
-          nTransverse_leadtrack++;
-          ptSumTransverse_leadtrack += pT;
-          nTransverse2_leadtrack++;
-          ptSumTransverse2_leadtrack += pT;
-
-        }
-
-      if (fabs(dphi_leadtrack) < PI/3.) {   // Transverse region
-          nTowards_leadtrack++;
-          ptSumTowards_leadtrack += pT;
-        }
-
-     if (fabs(dphi_leadtrack) > 2.*PI/3.) {   // Transverse region
-          nAway_leadtrack++;
-          ptSumAway_leadtrack += pT;
-        }
-
-     }//track found
-
-
-   }//Loop over particles
+	}//track found
+      }//Loop over particles
 
       const double fullarea = 8./3. * PI;
       const double halfarea = 4./3. * PI;
-
+      
       if (!p_leadjet.isZero()){
-
-      if (nTransverse2_leadjet > nTransverse1_leadjet){
-       nTransverseMax_leadjet = nTransverse2_leadjet;
-       nTransverseMin_leadjet = nTransverse1_leadjet;
-       } else{
-       nTransverseMax_leadjet = nTransverse1_leadjet;
-       nTransverseMin_leadjet = nTransverse2_leadjet;
-       }
-
-      if (ptSumTransverse2_leadjet > ptSumTransverse1_leadjet){
-       ptSumTransverseMax_leadjet = ptSumTransverse2_leadjet;
-       ptSumTransverseMin_leadjet = ptSumTransverse1_leadjet;
-       }
-       else{
-       ptSumTransverseMax_leadjet = ptSumTransverse1_leadjet;
-       ptSumTransverseMin_leadjet = ptSumTransverse2_leadjet;
-       }
-
-
-      _h_Nch_vs_leadJetPt_transDiff->fill(pTleadjet/GeV, 1./halfarea*(nTransverseMax_leadjet - nTransverseMin_leadjet), weight);
-      _h_PtSum_vs_leadJetPt_transDiff->fill(pTleadjet/GeV, 1./halfarea*(ptSumTransverseMax_leadjet - ptSumTransverseMin_leadjet), weight);
-      _h_Nch_vs_leadJetPt_transAvg->fill(pTleadjet/GeV, 1./fullarea*(nTransverseMax_leadjet + nTransverseMin_leadjet), weight);
-      _h_PtSum_vs_leadJetPt_transAvg->fill(pTleadjet/GeV, 1./fullarea*(ptSumTransverseMax_leadjet + ptSumTransverseMin_leadjet), weight);
-      _h_Nch_vs_leadJetPt_transMax->fill(pTleadjet/GeV, 1./halfarea*nTransverseMax_leadjet, weight);
-      _h_PtSum_vs_leadJetPt_transMax->fill(pTleadjet/GeV, 1./halfarea*ptSumTransverseMax_leadjet, weight);
-      _h_Nch_vs_leadJetPt_transMin->fill(pTleadjet/GeV, 1./halfarea*nTransverseMin_leadjet, weight);
-      _h_PtSum_vs_leadJetPt_transMin->fill(pTleadjet/GeV, 1./halfarea*ptSumTransverseMin_leadjet, weight);
-
+	
+	if (nTransverse2_leadjet > nTransverse1_leadjet){
+	  nTransverseMax_leadjet = nTransverse2_leadjet;
+	  nTransverseMin_leadjet = nTransverse1_leadjet;
+	} else{
+	  nTransverseMax_leadjet = nTransverse1_leadjet;
+	  nTransverseMin_leadjet = nTransverse2_leadjet;
+	}
+	
+	if (ptSumTransverse2_leadjet > ptSumTransverse1_leadjet){
+	  ptSumTransverseMax_leadjet = ptSumTransverse2_leadjet;
+	  ptSumTransverseMin_leadjet = ptSumTransverse1_leadjet;
+	}
+	else{
+	  ptSumTransverseMax_leadjet = ptSumTransverse1_leadjet;
+	  ptSumTransverseMin_leadjet = ptSumTransverse2_leadjet;
+	}
+	
+	_h_Nch_vs_leadJetPt_transDiff->fill(pTleadjet/GeV, 1./halfarea*(nTransverseMax_leadjet - nTransverseMin_leadjet), weight);
+	_h_PtSum_vs_leadJetPt_transDiff->fill(pTleadjet/GeV, 1./halfarea*(ptSumTransverseMax_leadjet - ptSumTransverseMin_leadjet), weight);
+	_h_Nch_vs_leadJetPt_transAvg->fill(pTleadjet/GeV, 1./fullarea*(nTransverseMax_leadjet + nTransverseMin_leadjet), weight);
+	_h_PtSum_vs_leadJetPt_transAvg->fill(pTleadjet/GeV, 1./fullarea*(ptSumTransverseMax_leadjet + ptSumTransverseMin_leadjet), weight);
+	_h_Nch_vs_leadJetPt_transMax->fill(pTleadjet/GeV, 1./halfarea*nTransverseMax_leadjet, weight);
+	_h_PtSum_vs_leadJetPt_transMax->fill(pTleadjet/GeV, 1./halfarea*ptSumTransverseMax_leadjet, weight);
+	_h_Nch_vs_leadJetPt_transMin->fill(pTleadjet/GeV, 1./halfarea*nTransverseMin_leadjet, weight);
+	_h_PtSum_vs_leadJetPt_transMin->fill(pTleadjet/GeV, 1./halfarea*ptSumTransverseMin_leadjet, weight);
+	
       } //for leading jet
-
+      
       if (!p_leadtrack.isZero()){
 
-      if (nTransverse2_leadtrack > nTransverse1_leadtrack){
-       nTransverseMax_leadtrack = nTransverse2_leadtrack;
-       nTransverseMin_leadtrack = nTransverse1_leadtrack;
-       } else{
-       nTransverseMax_leadtrack = nTransverse1_leadtrack;
-       nTransverseMin_leadtrack = nTransverse2_leadtrack;
-       }
+	if (nTransverse2_leadtrack > nTransverse1_leadtrack){
+	  nTransverseMax_leadtrack = nTransverse2_leadtrack;
+	  nTransverseMin_leadtrack = nTransverse1_leadtrack;
+	} else{
+	  nTransverseMax_leadtrack = nTransverse1_leadtrack;
+	  nTransverseMin_leadtrack = nTransverse2_leadtrack;
+	}
+	
+	if (ptSumTransverse2_leadtrack > ptSumTransverse1_leadtrack){
+	  ptSumTransverseMax_leadtrack = ptSumTransverse2_leadtrack;
+	  ptSumTransverseMin_leadtrack = ptSumTransverse1_leadtrack;
+	}
+	else{
+	  ptSumTransverseMax_leadtrack = ptSumTransverse1_leadtrack;
+	  ptSumTransverseMin_leadtrack = ptSumTransverse2_leadtrack;
+	}
 
-      if (ptSumTransverse2_leadtrack > ptSumTransverse1_leadtrack){
-       ptSumTransverseMax_leadtrack = ptSumTransverse2_leadtrack;
-       ptSumTransverseMin_leadtrack = ptSumTransverse1_leadtrack;
-       }
-       else{
-       ptSumTransverseMax_leadtrack = ptSumTransverse1_leadtrack;
-       ptSumTransverseMin_leadtrack = ptSumTransverse2_leadtrack;
-       }
-
-      _h_Nch_vs_leadTrackPt_transDiff->fill(pTleadtrack/GeV, 1./halfarea*(nTransverseMax_leadtrack - nTransverseMin_leadtrack), weight);
-      _h_PtSum_vs_leadTrackPt_transDiff->fill(pTleadtrack/GeV, 1./halfarea*(ptSumTransverseMax_leadtrack - ptSumTransverseMin_leadtrack), weight);
-      _h_Nch_vs_leadTrackPt_transAvg->fill(pTleadtrack/GeV, 1./fullarea*(nTransverseMax_leadtrack + nTransverseMin_leadtrack), weight);
-      _h_PtSum_vs_leadTrackPt_transAvg->fill(pTleadtrack/GeV, 1./fullarea*(ptSumTransverseMax_leadtrack + ptSumTransverseMin_leadtrack), weight);
-
-      _h_Nch_vs_leadTrackPt_transMax->fill(pTleadtrack/GeV, 1./halfarea*nTransverseMax_leadtrack, weight);
-      _h_PtSum_vs_leadTrackPt_transMax->fill(pTleadtrack/GeV, 1./halfarea*ptSumTransverseMax_leadtrack, weight);
-      _h_Nch_vs_leadTrackPt_transMin->fill(pTleadtrack/GeV, 1./halfarea*nTransverseMin_leadtrack, weight);
-      _h_PtSum_vs_leadTrackPt_transMin->fill(pTleadtrack/GeV, 1./halfarea*ptSumTransverseMin_leadtrack, weight);
+	_h_Nch_vs_leadTrackPt_transDiff->fill(pTleadtrack/GeV, 1./halfarea*(nTransverseMax_leadtrack - nTransverseMin_leadtrack), weight);
+	_h_PtSum_vs_leadTrackPt_transDiff->fill(pTleadtrack/GeV, 1./halfarea*(ptSumTransverseMax_leadtrack - ptSumTransverseMin_leadtrack), weight);
+	_h_Nch_vs_leadTrackPt_transAvg->fill(pTleadtrack/GeV, 1./fullarea*(nTransverseMax_leadtrack + nTransverseMin_leadtrack), weight);
+	_h_PtSum_vs_leadTrackPt_transAvg->fill(pTleadtrack/GeV, 1./fullarea*(ptSumTransverseMax_leadtrack + ptSumTransverseMin_leadtrack), weight);
+	
+	_h_Nch_vs_leadTrackPt_transMax->fill(pTleadtrack/GeV, 1./halfarea*nTransverseMax_leadtrack, weight);
+	_h_PtSum_vs_leadTrackPt_transMax->fill(pTleadtrack/GeV, 1./halfarea*ptSumTransverseMax_leadtrack, weight);
+	_h_Nch_vs_leadTrackPt_transMin->fill(pTleadtrack/GeV, 1./halfarea*nTransverseMin_leadtrack, weight);
+	_h_PtSum_vs_leadTrackPt_transMin->fill(pTleadtrack/GeV, 1./halfarea*ptSumTransverseMin_leadtrack, weight);
 
       }//for leading track
-
-
-
-
-
-
-
-
     }
-
-
-
+    
+     
     /// Normalise histograms etc., after the run
     void finalize() {
     }
-
-
-
+    
+    
+    
   private:
-
-
+    
+    
     Profile1DPtr _h_Nch_vs_leadJetPt_transMax;
     Profile1DPtr _h_PtSum_vs_leadJetPt_transMax;
     Profile1DPtr _h_Nch_vs_leadJetPt_transMin;
@@ -295,7 +257,7 @@ namespace Rivet {
     Profile1DPtr _h_PtSum_vs_leadJetPt_transDiff;
     Profile1DPtr _h_Nch_vs_leadJetPt_transAvg;
     Profile1DPtr _h_PtSum_vs_leadJetPt_transAvg;
-
+    
     Profile1DPtr _h_Nch_vs_leadTrackPt_transMax;
     Profile1DPtr _h_PtSum_vs_leadTrackPt_transMax;
     Profile1DPtr _h_Nch_vs_leadTrackPt_transMin;
@@ -305,10 +267,10 @@ namespace Rivet {
     Profile1DPtr _h_Nch_vs_leadTrackPt_transAvg;
     Profile1DPtr _h_PtSum_vs_leadTrackPt_transAvg;
 
-
+    
   };
-
-
+  
+  
   // This global object acts as a hook for the plugin system
   DECLARE_RIVET_PLUGIN(CMS_PAS_FSQ_15_007);
 }

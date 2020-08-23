e01c6b4d5bfc50fd007b3995c376ed405a6fa998
Matthias <mkomm@cern.ch>
2020-08-23 18:33:58 +0200

    up

--- TOP/data/CMS_2019_I1744604.plot
+++ TOP/data/CMS_2019_I1744604.plot
@@ -1,5 +1,13 @@
-BEGIN PLOT /CMS_2019_I1744604/d13-x01-y01
-Title=Differential absolute cross section as a function of the particle-level top quark $p_{T}$
-XLabel=Particle-level top quark $p_{T}$ (GeV)
+BEGIN PLOT /CMS_2019_I1744604/d17-x01-y01
+Title=Absolute cross section, particle-level charged lepton $p_{T}$
+XLabel=Particle-level lepton $p_{T}$ (GeV)
 YLabel=$\textrm{d}\sigma_{\textrm{t+}\bar{\textrm{t}}}/\textrm{d}p_\textrm{T}$
 END PLOT
+
+BEGIN PLOT /CMS_2019_I1744604/d19-x01-y01
+Title=Absolute cross section, particle-level charged lepton rapidity
+XLabel=Particle-level lepton rapidity
+YLabel=$\textrm{d}\sigma_{\textrm{t+}\bar{\textrm{t}}}/\textrm{d}|y|$
+LogY=0
+END PLOT
+
--- TOP/src/CMS_2019_I1744604.cc
+++ TOP/src/CMS_2019_I1744604.cc
@@ -1,4 +1,3 @@
-// -*- C++ -*-
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/FastJets.hh"
@@ -13,8 +12,7 @@ namespace Rivet {
   class CMS_2019_I1744604 : public Analysis {
   private:
 
-    static std::vector<double> EquationSolve(double a, double b, double c, double d)
-    {
+    static std::vector<double> EquationSolve(double a, double b, double c, double d) {
       std::vector<double> result;
       
       std::complex<double> x1;
@@ -71,8 +69,7 @@ namespace Rivet {
     static std::pair<FourMomentum,FourMomentum> NuMomentum(
       double leptonPx, double leptonPy, double leptonPz, double leptonPt, 
       double leptonE, double metPx, double metPy
-    )
-    {
+    ) {
       double  mW = 80.399;
 
       FourMomentum result(0,0,0,0);
@@ -188,12 +185,54 @@ namespace Rivet {
       }
       return std::make_pair(result,result2);
     }
+    
+    static void fillAbsHist(Histo1DPtr& hist_abs, const Histo1DPtr& hist_t, const Histo1DPtr& hist_tbar) {
+      (*hist_abs)+=(*hist_t);
+      (*hist_abs)+=(*hist_tbar);
+      /*
+      for (size_t i = 0; i < hist_abs->numBins(); ++i) {
+        const auto& t_bin = hist_t->bin(i);
+        const auto& tbar_bin = hist_tbar->bin(i);
+        hist_abs->bin(i).fillBin(t_bin.height()+tbar_bin.height());
+      }
+      */
+    }
+    
+    static void fillNormHist(Histo1DPtr& hist_norm, const Histo1DPtr& hist_t, const Histo1DPtr& hist_tbar) {
+      for (size_t i = 0; i < hist_norm->numBins(); ++i) {
+        const auto& t_bin = hist_t->bin(i);
+        const auto& tbar_bin = hist_tbar->bin(i);
+        hist_norm->bin(i).fillBin(t_bin.height()+tbar_bin.height());
+      }
+    }
+    
+    static void fillRatioHist(Histo1DPtr& hist_ratio, const Histo1DPtr& hist_t, const Histo1DPtr& hist_tbar) {
+      for (size_t i = 0; i < hist_ratio->numBins(); ++i) {
+        const auto& t_bin = hist_t->bin(i);
+        const auto& tbar_bin = hist_tbar->bin(i);
+        hist_ratio->bin(i).fillBin(t_bin.height()/(t_bin.height()+tbar_bin.height()));
+      }
+    }
+    
+    
+    static double calcXsec(const Histo1DPtr& hist) {
+      double xsec = 0.;
+      for (size_t i = 0; i < hist->numBins(); ++i) {
+        auto bin = hist->bin(i);
+        xsec+=bin.height()*bin.width();
+      }
+      return xsec;
+    }
   
   
   public:
+    CMS_2019_I1744604(): 
+      Analysis("CMS_2019_I1744604"),
+      _sum_t_weights(0.),
+      _sum_tbar_weights(0.) {
+    }
 
-    /// Constructor
-    DEFAULT_RIVET_ANALYSIS_CTOR(CMS_2019_I1744604);
+    
 
 
     void init() {
@@ -201,7 +240,7 @@ namespace Rivet {
       Cut particle_cut = (Cuts::abseta < 5.0) and (Cuts::pT > 0.*MeV);
       Cut lepton_cut   = (Cuts::abseta < 2.4) and (Cuts::pT > 26.*GeV);
       
-      // Generic final state
+
       FinalState fs(particle_cut);
 
       // Dressed leptons
@@ -227,7 +266,7 @@ namespace Rivet {
       VetoedFinalState fsForJets(fs);
       fsForJets.addVetoOnThisFinalState(dressed_leptons);
       declare(
-        //excludes all neutrinos by default
+        // excludes all neutrinos by default
         FastJets(fsForJets, FastJets::ANTIKT, 0.4), 
         "Jets"
       );
@@ -240,53 +279,74 @@ namespace Rivet {
       prompt_neutrinos.acceptTauDecays(true);
       declare(prompt_neutrinos, "Neutrinos");
       
-      //Partonic top (for differentiating between t and tbar events only)
+      // Partonic top (for differentiating between t and tbar events only)
       declare(PartonicTops(),"TopQuarks");
-      
 
       book(_hist_abs_top_pt,"d13-x01-y01",_binning_top_pt);
       book(_hist_norm_top_pt,"d37-x01-y01",_binning_top_pt);
       book(_hist_ratio_top_pt,"d59-x01-y01",_binning_top_pt);
-
-
       book(_hist_t_top_pt,"t_top_pt",_binning_top_pt);
       book(_hist_tbar_top_pt,"tbar_top_pt",_binning_top_pt);
-
       
-
-      //book(_hist_abs_top_pt,"d13-x01-y01");
-      //top y 0.,0.2,0.5,0.8,1.3,2.6
-      //l pt 26.,35.,45.,60.,85.,200.
-      //l y 0.0,0.4,0.8,1.5,1.9,2.4
-      //w pt 0.,35.,55.,80.,140.,250.
-      //cos -1.0,-0.6,-0.3,0.0,0.3,0.6,1.0
-
-      /*
-      // Book histograms
-      // specify custom binning
-      book(_h["XXXX"], "myh1", 20, 0.0, 100.0);
-      book(_h["YYYY"], "myh2", logspace(20, 1e-2, 1e3));
-      book(_h["ZZZZ"], "myh3", {0.0, 1.0, 2.0, 4.0, 8.0, 16.0});
-      // take binning from reference data using HEPData ID (digits in "d01-x01-y01" etc.)
-      book(_h["AAAA"], 1, 1, 1);
-      book(_p["BBBB"], 2, 1, 1);
-      book(_c["CCCC"], 3, 1, 1);
-      */
-
+      book(_hist_abs_top_y,"d15-x01-y01",_binning_top_y);
+      book(_hist_norm_top_y,"d39-x01-y01",_binning_top_y);
+      book(_hist_ratio_top_y,"d61-x01-y01",_binning_top_y);
+      book(_hist_t_top_y,"t_top_y",_binning_top_y);
+      book(_hist_tbar_top_y,"tbar_top_y",_binning_top_y);
+            
+      book(_hist_abs_lepton_pt,"d17-x01-y01",_binning_lepton_pt);
+      book(_hist_norm_lepton_pt,"d41-x01-y01",_binning_lepton_pt);
+      book(_hist_ratio_lepton_pt,"d63-x01-y01",_binning_lepton_pt);
+      book(_hist_t_lepton_pt,"t_lepton_pt",_binning_lepton_pt);
+      book(_hist_tbar_lepton_pt,"tbar_lepton_pt",_binning_lepton_pt);
+      
+      book(_hist_abs_lepton_y,"d19-x01-y01",_binning_lepton_y);
+      book(_hist_norm_lepton_y,"d43-x01-y01",_binning_lepton_y);
+      book(_hist_ratio_lepton_y,"d65-x01-y01",_binning_lepton_y);
+      book(_hist_t_lepton_y,"t_lepton_y",_binning_lepton_y);
+      book(_hist_tbar_lepton_y,"tbar_lepton_y",_binning_lepton_y);
+      
+      book(_hist_abs_w_pt,"d21-x01-y01",_binning_w_pt);
+      book(_hist_norm_w_pt,"d45-x01-y01",_binning_w_pt);
+      book(_hist_ratio_w_pt,"d67-x01-y01",_binning_w_pt);
+      book(_hist_t_w_pt,"t_w_pt",_binning_w_pt);
+      book(_hist_tbar_w_pt,"tbar_w_pt",_binning_w_pt);
+      
+      book(_hist_abs_top_cos,"d23-x01-y01",_binning_top_cos);
+      book(_hist_norm_top_cos,"d47-x01-y01",_binning_top_cos);
+      book(_hist_t_top_cos,"t_top_cos",_binning_top_cos);
+      book(_hist_tbar_top_cos,"tbar_top_cos",_binning_top_cos);
+      
     }
 
 
-    /// Perform the per-event analysis
     void analyze(const Event& event) {
-
+      vector<Particle> topQuarks = applyProjection<PartonicTops>(
+        event,
+        "TopQuarks"
+      ).tops();
+      
+      if (topQuarks.size() != 1) {
+        return;
+      }
+      
+      if (topQuarks[0].charge() > 0) {
+        _sum_t_weights += 1.;
+      } else {
+        _sum_tbar_weights += 1.;
+      }
+      
+      
       vector<DressedLepton> leptons = applyProjection<DressedLeptons>(
         event,
         "DressedLeptons"
       ).dressedLeptons();
       
+      
       if (leptons.size() != 1) {
         return;
       }
+      std::cout<<leptons[0].pid()<<std::endl;
       
       Cut jet_cut((Cuts::abseta < 4.7) and (Cuts::pT > 40.*GeV));
       vector<Jet> jets = applyProjection<FastJets>(
@@ -294,7 +354,15 @@ namespace Rivet {
         "Jets"
       ).jets(jet_cut);
       
-      if (jets.size() != 2) {
+      std::vector<Jet> cleanedJets;
+      DeltaRLess dRFct(leptons[0],0.4);
+      for (const auto& jet: jets) {
+        if (not dRFct(jet)) {
+          cleanedJets.push_back(jet);
+        }
+      }
+      
+      if (cleanedJets.size() != 2) {
         return;
       }
       
@@ -307,125 +375,138 @@ namespace Rivet {
         return;
       }
 
-      vector<Particle> topQuarks = applyProjection<PartonicTops>(
-        event,
-        "TopQuarks"
-      ).tops();
       
-      if (topQuarks.size() != 1) {
-        return;
-      }
-
-      std::cout<<"Top quarks: "<<topQuarks.size()<<std::endl;
-      for (size_t i = 0; i < topQuarks.size(); ++i)
-      {
-        std::cout<<" - "<<topQuarks[i].pid()<<", "<<topQuarks[i].pt()<<std::endl;
-      }
-      
-
-      _hist_t_top_pt->fill(leptons[0].pt()/GeV);
-      _hist_tbar_top_pt->fill(leptons[0].pt()/GeV);
-
-      /*
-      Cut jet_cut = (Cuts::abseta < _jetMaxEta) and (Cuts::pT > _jetMinPt*GeV);
-      _jets = applyProjection<FastJets>(event, "Jets").jetsByPt(jet_cut);
-      for (const Jet& jet : _jets) {
-        if (jet.bTagged()) _bjets.push_back(jet);
-        else               _ljets.push_back(jet);
-      }
-
-      _nujets = applyProjection<FastJets>(event, "NuJets").jetsByPt(jet_cut);
-
-      Cut fatjet_cut = (Cuts::abseta < _fatJetMaxEta) and (Cuts::pT > _fatJetMinPt*GeV);
-      _fatjets = applyProjection<FastJets>(event, "FatJets").jetsByPt(fatjet_cut);
-
-      _photons = applyProjection<FinalState>(event, "Photons").particlesByPt();
-
-      _neutrinos = applyProjection<FinalState>(event, "Neutrinos").particlesByPt();
-
-      _met = -applyProjection<MissingMomentum>(event, "MET").vectorEt();
-
-
-
-
-      // the final-state particles declared above are clustered using FastJet with
-      // the anti-kT algorithm and a jet-radius parameter 0.4
-      // muons and neutrinos are excluded from the clustering
-      FastJets jetfs(fs, FastJets::ANTIKT, 0.4, JetAlg::Muons::NONE, JetAlg::Invisibles::NONE);
-      declare(jetfs, "jets");
-
-      // FinalState of prompt photons and bare muons and electrons in the event
-      PromptFinalState photons(Cuts::abspid == PID::PHOTON);
-      PromptFinalState bare_leps(Cuts::abspid == PID::MUON || Cuts::abspid == PID::ELECTRON);
-
-      // dress the prompt bare leptons with prompt photons within dR < 0.1
-      // apply some fiducial cuts on the dressed leptons
-      Cut lepton_cuts = Cuts::abseta < 2.5 && Cuts::pT > 20*GeV;
-      DressedLeptons dressed_leps(photons, bare_leps, 0.1, lepton_cuts);
-
-      /// @todo Do the event by event analysis here
-
-      // retrieve dressed leptons, sorted by pT
-      vector<DressedLepton> leptons = apply<DressedLeptons>(event, "leptons").dressedLeptons();
-
-      // retrieve clustered jets, sorted by pT, with a minimum pT cut
-      Jets jets = apply<FastJets>(event, "jets").jetsByPt(Cuts::pT > 30*GeV);
-
-      // remove all jets within dR < 0.2 of a dressed lepton
-      idiscardIfAnyDeltaRLess(jets, leptons, 0.2);
-
-      // select jets ghost-associated to B-hadrons with a certain fiducial selection
-      Jets bjets = filter_select(jets, [](const Jet& jet) {
-        return  jet.bTagged(Cuts::pT > 5*GeV && Cuts::abseta < 2.5);
-      });
-
-      // veto event if there are no b-jets
-      if (bjets.empty())  vetoEvent;
-
-      // apply a missing-momentum cut
-      if (apply<MissingMomentum>(event, "MET").missingPt() < 30*GeV)  vetoEvent;
-      */
-      // fill histogram with leading b-jet pT
-      //_h["XXXX"]->fill(bjets[0].pT()/GeV);
-
-    }
-
-
-    /// Normalise histograms etc., after the run
-    void finalize() {
-
-      //normalize(_h["YYYY"]); // normalize to unity
-      scale(_hist_t_top_pt, crossSection()/picobarn/sumOfWeights()); // norm to cross section
-      scale(_hist_tbar_top_pt, crossSection()/picobarn/sumOfWeights()); // norm to cross section
-      
-      for (size_t ipt = 0; ipt < _hist_t_top_pt->numBins(); ++ ipt) {
-        const auto& t_bin = _hist_t_top_pt->bin(ipt);
-        const auto& tbar_bin = _hist_tbar_top_pt->bin(ipt);
-        auto& abs_bin = _hist_abs_top_pt->bin(ipt);
-        auto& norm_bin = _hist_norm_top_pt->bin(ipt);
-        auto& ratio_bin = _hist_ratio_top_pt->bin(ipt);
+      if (topQuarks[0].charge() > 0) {
+        _hist_t_lepton_pt->fill(leptons[0].pt()/GeV);
+        _hist_t_lepton_y->fill(leptons[0].absrapidity());
         
-        abs_bin.fillBin(t_bin.height()+tbar_bin.height());
-        norm_bin.fillBin(t_bin.height()+tbar_bin.height());
-        ratio_bin.fillBin(t_bin.height()/(t_bin.height()+tbar_bin.height()));
+      } else {
+        _hist_tbar_lepton_pt->fill(leptons[0].pt()/GeV);
+        _hist_tbar_lepton_y->fill(leptons[0].absrapidity());
       }
     }
 
-    static const std::vector<double> _binning_top_pt; 
-     
-    Histo1DPtr _hist_t_top_pt;
-    Histo1DPtr _hist_tbar_top_pt;
+    void finalize() {
+      std::cout<<"weights="<<sumOfWeights()<<", t="<<_sum_t_weights<<", tbar="<<_sum_tbar_weights<<", xsec="<<(crossSection()/picobarn)<<std::endl;
+      std::cout<<"int (w/o overflow)="<<_hist_t_lepton_pt->integral(false)<<", int="<<_hist_t_lepton_pt->integral(true)<<", xsec="<<(0.5*_hist_t_lepton_pt->integral(false)*_t_xsec_fraction*crossSection()/picobarn/_sum_t_weights)<<std::endl;
+      std::cout<<"int (w/o overflow)="<<_hist_tbar_lepton_pt->integral(false)<<", int="<<_hist_tbar_lepton_pt->integral(true)<<", xsec="<<(0.5*_hist_tbar_lepton_pt->integral(false)*_tbar_xsec_fraction*crossSection()/picobarn/_sum_tbar_weights)<<std::endl;
+      
+
+      scale(_hist_t_top_pt, 0.5*_t_xsec_fraction*crossSection()/picobarn/_sum_t_weights);
+      scale(_hist_tbar_top_pt, 0.5*_tbar_xsec_fraction*crossSection()/picobarn/sumOfWeights());
+      
+      scale(_hist_t_top_y, 0.5*_t_xsec_fraction*crossSection()/picobarn/_sum_t_weights);
+      scale(_hist_tbar_top_y, 0.5*_tbar_xsec_fraction*crossSection()/picobarn/_sum_tbar_weights);
+      
+      scale(_hist_t_lepton_pt, 0.5*_t_xsec_fraction*crossSection()/picobarn/_sum_t_weights);
+      scale(_hist_tbar_lepton_pt, 0.5*_tbar_xsec_fraction*crossSection()/picobarn/_sum_tbar_weights);
+      
+      scale(_hist_t_lepton_y, 0.5*_t_xsec_fraction*crossSection()/picobarn/_sum_t_weights);
+      scale(_hist_tbar_lepton_y, 0.5*_tbar_xsec_fraction*crossSection()/picobarn/_sum_tbar_weights);
+
+      scale(_hist_t_w_pt,0.5*_t_xsec_fraction*crossSection()/picobarn/_sum_t_weights);
+      scale(_hist_tbar_w_pt, 0.5*_tbar_xsec_fraction*crossSection()/picobarn/_sum_tbar_weights);
+      
+      scale(_hist_t_top_cos, 0.5*_t_xsec_fraction*crossSection()/picobarn/_sum_t_weights);
+      scale(_hist_tbar_top_cos, 0.5*_tbar_xsec_fraction*crossSection()/picobarn/_sum_tbar_weights);
+      
+      
+      fillAbsHist(_hist_abs_top_pt,_hist_t_top_pt,_hist_tbar_top_pt);
+      fillNormHist(_hist_norm_top_pt,_hist_t_top_pt,_hist_tbar_top_pt);
+      fillRatioHist(_hist_ratio_top_pt,_hist_t_top_pt,_hist_tbar_top_pt);
+      
+      fillAbsHist(_hist_abs_top_y,_hist_t_top_y,_hist_tbar_top_y);
+      fillNormHist(_hist_norm_top_y,_hist_t_top_y,_hist_tbar_top_y);
+      fillRatioHist(_hist_ratio_top_y,_hist_t_top_y,_hist_tbar_top_y);
+      
+      fillAbsHist(_hist_abs_lepton_pt,_hist_t_lepton_pt,_hist_tbar_lepton_pt);
+      fillNormHist(_hist_norm_lepton_pt,_hist_t_lepton_pt,_hist_tbar_lepton_pt);
+      fillRatioHist(_hist_ratio_lepton_pt,_hist_t_lepton_pt,_hist_tbar_lepton_pt);
+      
+      fillAbsHist(_hist_abs_lepton_y,_hist_t_lepton_y,_hist_tbar_lepton_y);
+      fillNormHist(_hist_norm_lepton_y,_hist_t_lepton_y,_hist_tbar_lepton_y);
+      fillRatioHist(_hist_ratio_lepton_y,_hist_t_lepton_y,_hist_tbar_lepton_y);
+      
+      fillAbsHist(_hist_abs_w_pt,_hist_t_w_pt,_hist_tbar_w_pt);
+      fillNormHist(_hist_norm_w_pt,_hist_t_w_pt,_hist_tbar_w_pt);
+      fillRatioHist(_hist_ratio_w_pt,_hist_t_w_pt,_hist_tbar_w_pt);
+      
+      fillAbsHist(_hist_abs_top_cos,_hist_t_top_cos,_hist_tbar_top_cos);
+      fillNormHist(_hist_norm_top_cos,_hist_t_top_cos,_hist_tbar_top_cos);
+      
+      std::cout<<"int xsec: lpt="<<calcXsec(_hist_abs_lepton_pt)<<", ly="<<calcXsec(_hist_abs_lepton_y)<<std::endl;
+    }
+
+    //calculated with Hathor v2.1 for a top quark mass of 172.5 GeV at NLO in QCD 
+    static constexpr double _t_xsec_fraction = 136.02/216.99;
+    static constexpr double _tbar_xsec_fraction = 80.95/216.99;
+    
+    static const std::vector<double> _binning_top_pt;
+    static const std::vector<double> _binning_top_y;
+    static const std::vector<double> _binning_lepton_pt;
+    static const std::vector<double> _binning_lepton_y;
+    static const std::vector<double> _binning_w_pt;
+    static const std::vector<double> _binning_top_cos;
+    
+    double _sum_t_weights;
+    double _sum_tbar_weights;
+    
     Histo1DPtr _hist_abs_top_pt;
     Histo1DPtr _hist_norm_top_pt;
     Histo1DPtr _hist_ratio_top_pt;
+    Histo1DPtr _hist_t_top_pt;
+    Histo1DPtr _hist_tbar_top_pt;
+    
+    Histo1DPtr _hist_abs_top_y;
+    Histo1DPtr _hist_norm_top_y;
+    Histo1DPtr _hist_ratio_top_y;
+    Histo1DPtr _hist_t_top_y;
+    Histo1DPtr _hist_tbar_top_y;
+    
+    Histo1DPtr _hist_abs_lepton_pt;
+    Histo1DPtr _hist_norm_lepton_pt;
+    Histo1DPtr _hist_ratio_lepton_pt;
+    Histo1DPtr _hist_t_lepton_pt;
+    Histo1DPtr _hist_tbar_lepton_pt;
+    
+    Histo1DPtr _hist_abs_lepton_y;
+    Histo1DPtr _hist_norm_lepton_y;
+    Histo1DPtr _hist_ratio_lepton_y;
+    Histo1DPtr _hist_t_lepton_y;
+    Histo1DPtr _hist_tbar_lepton_y;
+    
+    Histo1DPtr _hist_abs_w_pt;
+    Histo1DPtr _hist_norm_w_pt;
+    Histo1DPtr _hist_ratio_w_pt;
+    Histo1DPtr _hist_t_w_pt;
+    Histo1DPtr _hist_tbar_w_pt;
+    
+    Histo1DPtr _hist_abs_top_cos;
+    Histo1DPtr _hist_norm_top_cos;
+    Histo1DPtr _hist_t_top_cos;
+    Histo1DPtr _hist_tbar_top_cos;
   };
+
 
   const std::vector<double> CMS_2019_I1744604::_binning_top_pt{{
     0.,50.,80.,120.,180.,300.
   }};
+  const std::vector<double> CMS_2019_I1744604::_binning_top_y{{
+    0.,0.2,0.5,0.8,1.3,2.6
+  }};
+  const std::vector<double> CMS_2019_I1744604::_binning_lepton_pt{{
+    26.,35.,45.,60.,85.,200.
+  }};
+  const std::vector<double> CMS_2019_I1744604::_binning_lepton_y{{
+    0.0,0.4,0.8,1.5,1.9,2.4
+  }};
+  const std::vector<double> CMS_2019_I1744604::_binning_w_pt{{
+    0.,35.,55.,80.,140.,250.
+  }};
+  const std::vector<double> CMS_2019_I1744604::_binning_top_cos{{
+    -1.0,-0.6,-0.3,0.0,0.3,0.6,1.0
+  }};
 
-  // The hook for the plugin system
   DECLARE_RIVET_PLUGIN(CMS_2019_I1744604);
-
-
 }
+
